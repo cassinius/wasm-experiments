50dc16f3e83da7ba958648477ddb7402d83d3a50
Bernd Malle <bernd.malle@gmail.com>
2017-07-07 23:54:32 +0200

    correct data preparation for multiple subplots, one per algorithm

--- stats/plots/one_plot_per_algo.py
+++ stats/plots/one_plot_per_algo.py
@@ -24,8 +24,8 @@ INPUT_COLS.pop(0)
 N = len(INPUT_COLS)
 
 # Overall data
-algo_means = np.array(data.mean(axis=0))
-algo_std = np.array(data.std(axis=0))
+overall_means = np.array(data.mean(axis=0))
+overall_stds = np.array(data.std(axis=0))
 
 natives = ["Native C++ GCC", "Native C++ VS"]
 jss = ["native JS Node"]
@@ -52,18 +52,37 @@ for col in INPUT_COLS:
     wasm_means.append(np.mean(np.array(data.loc[data[K_FOLD_VAL].isin(wasms)][col])))
     wasm_stds.append(np.std(np.array(data.loc[data[K_FOLD_VAL].isin(wasms)][col])))
 
-# print(native_means)
-# print(js_means)
-# print(asmjs_means)
-# print(wasm_means)
+print(overall_means)
+print(native_means)
+print(js_means)
+print(asmjs_means)
+print(wasm_means)
+
+# We need to put the 10 entries into their respective arrays
+bars_means = {}
+for idx, col in enumerate(INPUT_COLS):
+    bars_means[col] = []
+    bars_means[col].append(native_means[idx])
+    bars_means[col].append(js_means[idx])
+    bars_means[col].append(asmjs_means[idx])
+    bars_means[col].append(wasm_means[idx])
+bars_stds = {}
+for idx, col in enumerate(INPUT_COLS):
+    bars_stds[col] = []
+    bars_stds[col].append(native_stds[idx])
+    bars_stds[col].append(js_stds[idx])
+    bars_stds[col].append(asmjs_stds[idx])
+    bars_stds[col].append(wasm_stds[idx])
+
+print(bars_means)
+print(bars_stds)
 
 x_range = np.arange(N)  # the x locations for the groups
-width = .17       # the width of the bars
+width = .3             # the width of the bars
 
 fig, ax = plt.subplots()
 
-
-overall = plt.barh(x_range, algo_means, width, color=colors[0], xerr=algo_std, log=True, label="overall")
+overall = plt.barh(x_range, overall_means, width, color=colors[0], xerr=overall_stds, log=True, label="overall")
 native = plt.barh(x_range+width, native_means, width, color=colors[1], xerr=native_stds, log=True, label="native")
 jss = plt.barh(x_range+2*width, js_means, width, color=colors[2], xerr=js_stds, log=True, label = "JavaScript")
 asms = plt.barh(x_range+3*width, asmjs_means, width, color=colors[3], xerr=asmjs_stds, log=True, label = "ASM.js")
@@ -79,7 +98,7 @@ plt.xlabel('Runtime in ms.')
 
 plt.title('Mean / Std. per runtime environment & algorithm')
 plt.yticks(x_range+0.34, INPUT_COLS, rotation=45, ha='right')
-plt.axis([0, max(algo_means + algo_std)*2, -0.25, N])
+plt.axis([0, max(overall_means + overall_stds) * 2, -0.25, N])
 # plt.subplots_adjust(bottom=0.3)
 
 
