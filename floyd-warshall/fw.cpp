d6a98b84706d4df6cd85435c65b5e373dd2d60e4
Bernd Malle <bernd.malle@gmail.com>
2019-03-30 01:19:58 +0100

    wrote DICT graph version in CPP - 100x slower than Arrays - can this be?

--- floyd-warshall/fw.cpp
+++ floyd-warshall/fw.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <map>
 
 using namespace std;
 
 typedef std::chrono::high_resolution_clock Clock;
 typedef std::chrono::milliseconds milliseconds;
 
-const unsigned int V = 1034;
+const unsigned int V = 250;
 unsigned int graph[V][V];
 unsigned int graphSingleArray[V*V];
+std::map<int, std::map<int, int>> graphDict;
+
+
+//=========================================
+//     DICTIONARY GRAPH FUNCTIONS
+//=========================================
+void generateGraphDict() {
+  for (unsigned int i = 0; i < V; ++i) {
+    for (unsigned int j = 0; j < V; ++j) {
+      graph[i][j] = i == j ? 0 : 1 + rand()%10;
+    }
+  }
+}
+
+void FWDenseDict() {
+  for (unsigned int k = 0; k < V; ++k) {
+    for (unsigned int i = 0; i < V; ++i) {
+      for (unsigned int j = 0; j < V; ++j) {
+        if (graphDict[i][j] > graphDict[i][k] + graphDict[k][j]) {
+          graphDict[i][j] = graphDict[i][k] + graphDict[k][j];
+        }
+      }
+    }
+  }
+}
 
 
 //=========================================
@@ -31,7 +57,7 @@ void generateGraphSingleArray() {
   }
 }
 
-void FWDenseSingleArray(unsigned int graph[V][V]) {
+void FWDenseSingleArray() {
   for (unsigned int k = 0; k < V; ++k) {
     for (unsigned int i = 0; i < V; ++i) {
       for (unsigned int j = 0; j < V; ++j) {
@@ -56,7 +82,7 @@ void generateGraphDoubleArray() {
   }
 }
 
-void FWDenseDoubleArray(unsigned int graph[V][V]) {
+void FWDenseDoubleArray() {
   for (unsigned int k = 0; k < V; ++k) {
     for (unsigned int i = 0; i < V; ++i) {
       for (unsigned int j = 0; j < V; ++j) {
@@ -69,6 +95,26 @@ void FWDenseDoubleArray(unsigned int graph[V][V]) {
 }
 
 
+void testDictGraphPerformance() {
+  Clock::time_point time_creation_start = Clock::now();
+  generateGraphDict();  
+  Clock::time_point time_creation_end = Clock::now();
+  milliseconds time_creation = std::chrono::duration_cast<milliseconds>( (time_creation_end - time_creation_start ) );
+  cout << "Time to construct DICT graph: " << time_creation.count() << " ms." << endl;
+
+  Clock::time_point time_10_fold_start = Clock::now();
+  for (unsigned int i = 0; i < 10; ++i) {
+    generateGraphDict();
+    FWDenseDict();
+    cout << "Completed FWDense iteration on DICT #" << i << endl;
+  }
+  Clock::time_point time_10_fold_stop = Clock::now();
+
+  milliseconds time_10_fold_mean = std::chrono::duration_cast<milliseconds>( (time_10_fold_stop - time_10_fold_start ) / 10 );
+  cout << "Time for Floyd Warshal on DICT (10-fold mean): " << time_10_fold_mean.count() << " ms." << endl;
+}
+
+
 void testSingleArrayGraphPerformance() {
   Clock::time_point time_creation_start = Clock::now();
   generateGraphSingleArray();  
@@ -78,7 +124,8 @@ void testSingleArrayGraphPerformance() {
 
   Clock::time_point time_10_fold_start = Clock::now();
   for (unsigned int i = 0; i < 10; ++i) {
-    FWDenseSingleArray(graph);
+    generateGraphSingleArray();
+    FWDenseSingleArray();
     cout << "Completed FWDense iteration on single array #" << i << endl;
   }
   Clock::time_point time_10_fold_stop = Clock::now();
@@ -97,7 +144,8 @@ void testDoubleArrayGraphPerformance() {
 
   Clock::time_point time_10_fold_start = Clock::now();
   for (unsigned int i = 0; i < 10; ++i) {
-    FWDenseDoubleArray(graph);
+    generateGraphDoubleArray();
+    FWDenseDoubleArray();
     cout << "Completed FWDense iteration on nested array #" << i << endl;
   }
   Clock::time_point time_10_fold_stop = Clock::now();
@@ -109,6 +157,7 @@ void testDoubleArrayGraphPerformance() {
 
 
 int main() {
+  testDictGraphPerformance();
   testSingleArrayGraphPerformance();
   testDoubleArrayGraphPerformance();
 }
