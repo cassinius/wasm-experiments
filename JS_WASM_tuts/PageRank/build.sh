8430140c4335abbe22f09bf2f2c783e478f664ec
Bernd Malle <bernd.malle@gmail.com>
2019-06-25 19:18:18 +0200

    can instantiate PR datastructs in CPP & call function from JS - still no good return value or algorithmic implementation

---
+++ JS_WASM_tuts/PageRank/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rm build/ -rf
+mkdir build
+cd build
+
+# _malloc and _free can be explicitly exported, but when
+# used with -s ONLY_MY_CODE=1 they become unavailable
+#
+# @todo take care of later...
+# 
+# -s ALLOW_MEMORY_GROWTH=1 \
+# -s ONLY_MY_CODE=1
+# -s SIDE_MODULE=1 \
+# -s EXPORTED_FUNCTIONS='["_sum_up"]' \
+#
+#
+em++ \
+  -O3 \
+  -g \
+  -std=c++11 \
+  --bind \
+  ../cpp/pr.cpp \
+  -s WASM=1 \
+  -s EXPORTED_FUNCTIONS='["_pagerank"]' \
+  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -o pr.js \
+  || exit 1
+
+# cmake ../cpp
+# make
+
+mv pr.js ../web/gen/
+mv pr.wasm ../web/gen/
+mv pr.wasm.map ../web/gen/
