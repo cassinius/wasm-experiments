9908bb4470da6e457ae23e3cdac94efee8d18dcb
Bernd Malle <bernd.malle@gmail.com>
2019-06-26 20:40:26 +0200

    (un)safety commit

--- JS_Wasm_Tuts_AS/floyd/assembly/floyd.ts
+++ JS_Wasm_Tuts_AS/floyd/assembly/floyd.ts
@@ -1,8 +1,27 @@
-/// <reference path="../../node_modules/assemblyscript/index.d.ts" />
+
 
 /**
- * 
- * @param graph 
+ * For memory access, we require an allocator
+ */
+import "allocator/tlsf";
+export { memory };
+
+
+
+/**
+ * Testing return types
+ */
+
+export function say42() : number {
+  return 42;
+}
+
+export function sayHi() : string {
+  return 'Hi from Wasm';
+}
+
+
+/**
  * 
  * @todo experiment with 1D array
  */
@@ -10,10 +29,11 @@ export function FW(graph: Array<Array<f32>>) : Array<Array<f32>> {
   // console.log('bla');
 
   let N: i32,
-    k: i32,
-    i: i32,
-    j: i32,
-    new_dist: f32;
+      k: i32,
+      i: i32,
+      j: i32,
+      new_dist: f32;
+
   N = graph.length;
   for ( k = 0; k < N; ++k ) {
     for ( i = 0; i < N; ++i ) {
