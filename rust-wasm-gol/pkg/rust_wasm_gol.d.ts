8bd3a1e6d19f2373509ea607bd13a979ba3e9d1f
Bernd Malle <bernd.malle@gmail.com>
2020-08-28 08:47:57 +0200

    updated rust-wasm-gol to newest rust nightly version (1.48) and js-sys version (0.3.44)

--- rust-wasm-gol/pkg/rust_wasm_gol.d.ts
+++ rust-wasm-gol/pkg/rust_wasm_gol.d.ts
@@ -1,40 +1,43 @@
 /* tslint:disable */
 /* eslint-disable */
+/**
+* Has cells & tmp_cells so we can build a swapchain
+*/
 export class Universe {
   free(): void;
 /**
 * Constructor
-* @param {number | undefined} width 
-* @param {number | undefined} height 
-* @returns {Universe} 
+* @param {number | undefined} width
+* @param {number | undefined} height
+* @returns {Universe}
 */
   static new(width?: number, height?: number): Universe;
 /**
 * Getters
-* @returns {number} 
+* @returns {number}
 */
   width(): number;
 /**
-* @returns {number} 
+* @returns {number}
 */
   height(): number;
 /**
-* @returns {number} 
+* @returns {number}
 */
   cells(): number;
 /**
 * Setters
-* @param {number} width 
+* @param {number} width
 */
   set_width(width: number): void;
 /**
-* @param {number} height 
+* @param {number} height
 */
   set_height(height: number): void;
 /**
 * Resets one / all cells to some state.
-* @param {number} row 
-* @param {number} col 
+* @param {number} row
+* @param {number} col
 */
   toggle_cell(row: number, col: number): void;
 /**
@@ -45,7 +48,7 @@ export class Universe {
   randomize_cells(): void;
 /**
 * Universe evolution
-* @param {number} nr_ticks 
+* @param {number} nr_ticks
 */
   ticks(nr_ticks: number): void;
 }
